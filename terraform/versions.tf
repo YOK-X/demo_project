76017e41b10dba11e694a4c2d5c3d7742100e6be
yok <info.autonika@gmail.com>
2022-02-19 19:13:02 +0200

    ЕУЫЕ

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -38,7 +38,5 @@ terraform {
 
 provider "aws" {
   region = "us-east-2"
-  shared_config_files     = "~/.aws/config"
-  shared_credentials_files= "~/.aws/credentials"
   profile                 = "Administrator"
 }
